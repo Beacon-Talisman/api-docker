ec9b5607693b35d6e3281b16cbddd5874cd5e156
Cristi Teodoreanu <cristi.teodoreanu@gmail.com>
2018-02-16 16:55:44 +0200

    Update run.sh

--- api/run.sh
+++ api/run.sh
@@ -6,6 +6,6 @@ docker run -d -it \
     --restart always \    
     -p 3000:3000 \
     -p 3306:3306 \
-    -v "/Projects/Vencuro/tal-api":/tal-api
+    -v "/Projects/Vencuro/tal-api":/tal-api \
     --name $DOCKER_CONTAINER \
     $DOCKER_IMAGE
