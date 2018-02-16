77cac9a909491948e172039795ab16815e454dcc
Cristi Teodoreanu <cristi.teodoreanu@gmail.com>
2018-02-16 16:54:33 +0200

    Mount local/docker tal-api folder

--- api/run.sh
+++ api/run.sh
@@ -3,8 +3,9 @@
 . common.sh
 
 docker run -d -it \
-    --restart always \
+    --restart always \    
     -p 3000:3000 \
     -p 3306:3306 \
+    -v "/Projects/Vencuro/tal-api":/tal-api
     --name $DOCKER_CONTAINER \
     $DOCKER_IMAGE
