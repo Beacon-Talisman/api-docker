7d133c96626e049ccafead8e44197d1a0f6d3a7e
root <root@docker.roweb.ro>
2018-02-19 13:34:20 +0200

    server fix changes for roweb dev

--- api/run.sh
+++ api/run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-. common.sh
+DOCKER_CONTAINER=talisman_api
+DOCKER_IMAGE=local/$DOCKER_CONTAINER:1.1
 
-docker run -d -it \
-    --cap-add SYS_ADMIN \
-    --restart always \    
-    -p 3000:3000 \
-    -p 33061:3306 \
-    -v "/home/vencuro/tal-api":/tal-api \
-    --name $DOCKER_CONTAINER \
-    $DOCKER_IMAGE
+
+#. common.sh
+
+docker run -d -it --cap-add SYS_ADMIN --expose 80  -v /sys/fs/cgroup:/sys/fs/cgroup:ro  --tmpfs /run  --tmpfs /run/lock --net nginx-proxy --restart always -p 33061:3306 -e VIRTUAL_HOST=vencuro-api.dock.roweb.ro -v "/home/vencuro/tal-api":/tal-api  --name $DOCKER_CONTAINER $DOCKER_IMAGE
