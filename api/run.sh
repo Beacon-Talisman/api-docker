103c39fbb13d409976f56d719e675d13278407bc
msandulescu <marius@roweb.ro>
2018-02-19 12:11:05 +0200

    initial commit

--- api/run.sh
+++ api/run.sh
@@ -3,9 +3,10 @@
 . common.sh
 
 docker run -d -it \
+    --cap-add SYS_ADMIN \
     --restart always \    
     -p 3000:3000 \
     -p 3306:3306 \
-    -v "/Projects/Vencuro/tal-api":/tal-api \
+    -v "./tal-api":/tal-api \
     --name $DOCKER_CONTAINER \
     $DOCKER_IMAGE
