e656570fee15356216d5cd1e576354607336402e
msandulescu <marius@roweb.ro>
2018-02-19 12:43:29 +0200

    change mysql port for external use

--- api/run.sh
+++ api/run.sh
@@ -6,7 +6,7 @@ docker run -d -it \
     --cap-add SYS_ADMIN \
     --restart always \    
     -p 3000:3000 \
-    -p 3306:3306 \
+    -p 33061:3306 \
     -v "/home/vencuro/tal-api":/tal-api \
     --name $DOCKER_CONTAINER \
     $DOCKER_IMAGE
