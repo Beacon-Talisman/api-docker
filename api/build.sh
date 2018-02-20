8b1badf842be18c11df07f4191459a1a237d51a7
msandulescu <marius@roweb.ro>
2018-02-20 19:08:33 +0200

    localhost docker container without volume mount

--- api/build.sh
+++ api/build.sh
@@ -2,7 +2,5 @@
 
 DOCKER_CONTAINER=vencuro_api
 DOCKER_IMAGE=local/$DOCKER_CONTAINER:1.1
-USERNAME=[put_your_github_username_here]
-PASSWORD=[put_your_github_password_here]
 
-docker build --force-rm -t $DOCKER_IMAGE --build-arg ssh_prv_key="$USERNAME" --build-arg ssh_pub_key="$PASSWORD" .
+docker build --force-rm -t $DOCKER_IMAGE .
--- api/run.sh
+++ api/run.sh
@@ -5,6 +5,7 @@ DOCKER_IMAGE=local/$DOCKER_CONTAINER:1.1
 
 # this is for localhost
 # docker run -d -it  --cap-add SYS_ADMIN --restart always -p 3000:3000 -p 3306:3306 -v "c:/Users/marius/Projects/Talisman/vencuro_dev":/tal-api --name $DOCKER_CONTAINER  $DOCKER_IMAGE
+docker run -d -it  --cap-add SYS_ADMIN --restart always -p 3000:3000 -p 3306:3306 --name $DOCKER_CONTAINER  $DOCKER_IMAGE
 
 # this is for roweb dev server
 docker run -d -it --cap-add SYS_ADMIN --expose 80  -v /sys/fs/cgroup:/sys/fs/cgroup:ro  --tmpfs /run  --tmpfs /run/lock --net nginx-proxy --restart always -p 33061:3306 -e VIRTUAL_HOST=vencuro-api.dock.roweb.ro -v "/home/vencuro/tal-api":/tal-api  --name $DOCKER_CONTAINER $DOCKER_IMAGE
